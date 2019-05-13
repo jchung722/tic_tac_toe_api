21670a575f90185d8510f48304448353493bda7d
Jessica Chung <jchung722@gmail.com>
2019-05-13 14:54:37 -0500

    send empty board json

--- lib/tic_tac_toe_api_web/controllers/page_controller.ex
+++ lib/tic_tac_toe_api_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule TicTacToeApiWeb.PageController do
   use TicTacToeApiWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    board = %Board{}
+    render(conn, "index.json", board: board)
   end
 end
--- lib/tic_tac_toe_api_web/views/page_view.ex
+++ lib/tic_tac_toe_api_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule TicTacToeApiWeb.PageView do
   use TicTacToeApiWeb, :view
+
+  def render("index.json", %{board: board}) do
+    %{spots: board.spots}
+  end
 end
