0f2c253a2fd9a1bf739951413ce31bc48621be0d
Tessa Anderso <quintessa@8thlight.com>
2019-05-24 17:17:30 -0500

    final state returning for a winning game

--- lib/tic_tac_toe_api_web/views/page_view.ex
+++ lib/tic_tac_toe_api_web/views/page_view.ex
@@ -9,6 +9,10 @@ defmodule TicTacToeApiWeb.PageView do
     case result do
       {:play, next_player, current_player, spots} ->
         %{result: "play", current_player: current_player, next_player: next_player, spots: spots}
+      {:win, winner, spots} ->
+        %{result: "wins", Winner: winner, spots: spots}
+      {:tie, _message, spots} ->
+        %{result: "TIE", spots: spots}
     end
   end
 end
--- test/tic_tac_toe_api_web/controllers/page_controller_test.exs
+++ test/tic_tac_toe_api_web/controllers/page_controller_test.exs
@@ -3,7 +3,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert json_response(conn, 200)["spots"] === ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    assert json_response(conn, 200)["spots"] === ["1", "2", "3",
+                                                  "4", "5", "6",
+                                                  "7", "8", "9"]
   end
 
   # test "GET /game", %{conn: conn} do
@@ -12,13 +14,39 @@ defmodule TicTacToeApiWeb.PageControllerTest do
   # end
 
   test "GET /game for new game", %{conn: conn} do
-    spots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    spots = "[\"1\", \"2\", \"3\", \"4\", \"5\", \"6\", \"7\", \"8\", \"9\"]"
     next_player = "X"
     current_player = "O"
     conn = get(conn, "/game", %{spots: spots, next_player: next_player, current_player: current_player})
     assert json_response(conn, 200)["result"] === "play"
     assert json_response(conn, 200)["next_player"] === "X"
     assert json_response(conn, 200)["current_player"] === "O"
-    assert json_response(conn, 200)["spots"] === ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    assert json_response(conn, 200)["spots"] === ["1", "2", "3",
+                                                  "4", "5", "6",
+                                                  "7", "8", "9"]
+  end
+
+  test "Get game for winning game", %{conn: conn} do
+    spots = "[\"X\", \"X\", \"X\", \"O\", \"X\", \"O\", \"O\", \"O\", \"X\"]"
+    next_player = "O"
+    current_player = "X"
+    conn = get(conn, "/game", %{spots: spots, next_player: next_player, current_player: current_player})
+    assert json_response(conn, 200)["result"] === "wins"
+    assert json_response(conn, 200)["Winner"] === "X"
+    assert json_response(conn, 200)["spots"] === ["X", "X", "X",
+                                                  "O", "X", "O",
+                                                  "O", "O", "X"]
+  end
+
+  test "Get game for tie game", %{conn: conn} do
+    spots = "[\"O\", \"X\", \"O\", \"X\", \"O\", \"X\", \"X\", \"O\", \"X\"]"
+    next_player = "O"
+    current_player = "X"
+    conn = get(conn, "/game", %{spots: spots, next_player: next_player, current_player: current_player})
+    assert json_response(conn, 200)["result"] === "TIE"
+    assert json_response(conn, 200)["spots"] === ["O", "X", "O",
+                                                  "X", "O", "X",
+                                                  "X", "O", "X"]
+
   end
 end
