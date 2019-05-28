844f5123b49ab490a9aea1646f7c899cc67faa7a
Tessa Anderso <quintessa@8thlight.com>
2019-05-28 11:18:27 -0500

    reformat test

--- test/tic_tac_toe_api_web/controllers/page_controller_test.exs
+++ test/tic_tac_toe_api_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule TicTacToeApiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
+
     assert json_response(conn, 200)["spots"] === ["1", "2", "3",
                                                   "4", "5", "6",
                                                   "7", "8", "9"]
@@ -12,7 +13,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
     spots = "[\"1\", \"2\", \"3\", \"4\", \"5\", \"6\", \"7\", \"8\", \"9\"]"
     next_player = "X"
     current_player = "O"
+
     conn = get(conn, "/status", %{spots: spots, next_player: next_player, current_player: current_player})
+
     assert json_response(conn, 200)["result"] === "play"
     assert json_response(conn, 200)["next_player"] === "X"
     assert json_response(conn, 200)["current_player"] === "O"
@@ -22,7 +25,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
     spots = "[\"X\", \"X\", \"X\", \"O\", \"X\", \"O\", \"O\", \"O\", \"X\"]"
     next_player = "O"
     current_player = "X"
+
     conn = get(conn, "/status", %{spots: spots, next_player: next_player, current_player: current_player})
+
     assert json_response(conn, 200)["result"] === "wins"
     assert json_response(conn, 200)["Winner"] === "X"
   end
@@ -31,7 +36,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
     spots = "[\"O\", \"X\", \"O\", \"X\", \"X\", \"O\", \"O\", \"X\", \"X\"]"
     next_player = "O"
     current_player = "X"
+
     conn = get(conn, "/status", %{spots: spots, next_player: next_player, current_player: current_player})
+
     assert json_response(conn, 200)["result"] === "wins"
     assert json_response(conn, 200)["Winner"] === "X"
   end
@@ -40,7 +47,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
     spots = "[\"O\", \"X\", \"O\", \"X\", \"O\", \"O\", \"O\", \"X\", \"X\"]"
     next_player = "X"
     current_player = "O"
+
     conn = get(conn, "/status", %{spots: spots, next_player: next_player, current_player: current_player})
+
     assert json_response(conn, 200)["result"] === "wins"
     assert json_response(conn, 200)["Winner"] === "O"
   end
@@ -49,7 +58,9 @@ defmodule TicTacToeApiWeb.PageControllerTest do
     spots = "[\"O\", \"X\", \"O\", \"X\", \"O\", \"X\", \"X\", \"O\", \"X\"]"
     next_player = "O"
     current_player = "X"
+
     conn = get(conn, "/status", %{spots: spots, next_player: next_player, current_player: current_player})
+
     assert json_response(conn, 200)["result"] === "TIE"
   end
 end
